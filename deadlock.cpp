ca500399c623d0b758f5c4ee8cfaa1d65984b7ed
segwitt <abhish014@gmail.com>
2019-04-20 14:29:17 +0530

    added comments

--- deadlock.cpp
+++ deadlock.cpp
@@ -7,6 +7,9 @@ using std::string;
 
 class A
 {
+
+	//ofstream f
+	// good to use this practice of bundling a sharable resource with mutex so that it is accessed only when locked
 	std::mutex _m1;
 	std::mutex _m2;
 
@@ -23,9 +26,13 @@ public:
 
 		std::lock_guard<std::mutex> locker2(_m2);
 		std::lock_guard<std::mutex> locker1(_m1);
+		// f << 
 		cout << "thread  id " << id << " value " << value << endl;
 
 	}
+
+	// ofstream getF(); // bad idea, access with muteex
+
 	~A() {} ;
 	
 };
@@ -39,6 +46,7 @@ int main(int argc, char const *argv[])
 	std::thread thread1(function1, std::ref(a) );
 	for (int i = 0; i < 100; ++i)
 	{
+		// accessing a resource wrapped along with mutex , good practice
 		a.print2(string("main "), i);
 	}
 	thread1.join();
@@ -46,4 +54,23 @@ int main(int argc, char const *argv[])
 	return 0;
 }
 
-// g++ -pthread -o main deadlock.cpp
+// to compile ,    g++ -pthread -o main deadlock.cpp
+
+/*
+
+A parameter to a thread is always passed by value
+to pass by ref we use std::ref() which creates a reference wrapper
+or we can move the value using std::move() (a thread can be only moved)
+
+
+mutex used for synchronisation
+
+each thread has identification number associated with it
+
+we can use mutex.lock() and mutex.unloK() but it is not exception safe
+we use lock guard so when ever it goes out of scope , whether exception or not , it is freed
+
+thread safey - a program is thread safe which processes data independently and wholly for different threads
+independent of context switches
+
+*/
